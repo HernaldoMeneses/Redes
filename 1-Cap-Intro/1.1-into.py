5ac43c5345897799e5b70b1d650ecf9eedd2fee0
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-04 14:57:58 -0300

    1.1-uso-redes.py

--- 1-Cap-Intro/1.1-into.py
+++ 1-Cap-Intro/1.1-into.py
@@ -1,3 +1,4 @@
+'''
 Século XVIII - Tecnologia Dominante: Sistemas mecânicos - precusores da Revolução Industrial
 Século XIX - Tecnologia Dominante: Máquinas a vapor - Revolução Industrial
 Século XX - Tecnologia Dominante: Informação - Revolução Informacional
@@ -24,3 +25,6 @@ Internet
 Sistema distribuído
     Várias máquinas interdependentes apresentan-se ao usuário como um sistema coerente
     EX World Wide Web
+'''
+#pag. 
+#file:///L:/Linear_/Base-Jump_04/Redes%20De%20Computadores%20-%20Tanenbaum%20-%205%C2%AA%20Edi%C3%A7%C3%A3o%20-%20pdf.pdf
---
+++ 1-Cap-Intro/1.1-uso-redes.py
@@ -0,0 +1,6 @@
+# Para que e para quem redes?
+
+aplicacoes_comerciais = {
+    'Compartilhamento de Recursos':'O objetivo é deixar todos os programas, equipamentos e, especialmente, dados ao alcance de todas as pessoas na rede, independentemente da localização física de recurso ou do usuário.',
+
+}
